6800deeb2eda0470a41ec7e462950d0167b713a9
gitInterface <97436826+gitInterface@users.noreply.github.com>
2024-01-11 15:20:05 +0800

    add Info, Login, HouseMain, Register1, Register2, Register3 vue add image

--- src/router/router.js
+++ src/router/router.js
@@ -6,6 +6,14 @@ import Search from "@views/functional/Search.vue";
 import NotFound from "@views/common/NotFound.vue";
 import Chatroom from "@views/functional/Chatroom.vue";
 import Order from "@views/common/Order.vue";
+import Info from "@views/common/Info.vue";
+import Login from "@views/common/Login.vue";
+import HouseMaintain from "@views/common/HouseMaintain.vue";
+import Register1 from "@views/common/Register1.vue";
+import Register2 from "@views/common/Register2.vue";
+import Register3 from "@views/common/Register3.vue";
+
+
 // 2. 定義路由
 const routes = [
   {
@@ -32,6 +40,36 @@ const routes = [
     path: "/order",
     name: "Order",
     component: Order,
+  },
+  {
+    path: "/info",
+    name: "Info",
+    component: Info,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/houseMaintain",
+    name: "HouseMaintain",
+    component: HouseMaintain,
+  },
+  {
+    path: "/register1",
+    name: "Register1",
+    component: Register1,
+  },
+  {
+    path: "/register2",
+    name: "Register2",
+    component: Register2,
+  },
+  {
+    path: "/register3",
+    name: "Register3",
+    component: Register3,
   }
 ];
 // 3. 建立路由實體並匯出
---
+++ src/views/common/Register1.vue
@@ -0,0 +1,102 @@
+<template>
+    <!-- Content Start -->
+    <div class="row">
+        <div class="col-lg-4 register-frame">
+            <div class="container-xxl bg-white p-5 register-form">
+                <p class="title">設定登入帳號</p>
+                <div class="register-icon">
+                    <img src="../../assets/images/icon-progress1.png" width="100px">
+                </div>
+                <br>
+                <p style="color: black;">請輸入Email或電話號碼</p>
+                <div class="accountDiv">
+                    <input type="email" class="account" name="account" placeholder="Email/電話號碼" required autofocus>
+                </div>
+                <br>
+                <br>
+                <div class="register-btn">
+                    <a href="index.html"><button class="btn btn-lg btn-dark btn-block" type="submit">驗證</button></a>
+                </div>
+                <br>
+                <hr>
+                <div class="google-btn">
+                    <button class="btn btn-outline-dark" type="submit"><img src="../../assets/images/icon-google.png"
+                            width="25px">&nbsp;透過Google帳號登入</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Content End -->
+</template>
+    
+<script setup>
+
+</script>
+    
+<style scoped>
+.row {
+    margin-top: 2em;
+}
+
+.register-frame {
+    margin: auto;
+    border: 1px solid black;
+    border-radius: 10px;
+    padding: 10px;
+    background-color: #d63384;
+}
+
+.register-form {
+    border: 1px solid black;
+    border-radius: 10px;
+}
+
+.title {
+    text-align: center;
+    color: black;
+    font-size: 25px;
+    margin-top: -30px;
+}
+
+.register-icon {
+    margin: auto;
+    text-align: center;
+}
+
+.accountDiv {
+    text-align: center;
+}
+
+input {
+    padding: 10px;
+    border-radius: 30px;
+}
+
+.account {
+    overflow: hidden;
+    width: 100%;
+}
+
+.register-btn {
+    margin: auto;
+    text-align: center;
+}
+
+.register-btn button {
+    background-color: #d63384;
+}
+
+.btn-dark {
+    border-radius: 50px;
+    color: white;
+}
+
+.google-btn {
+    margin: auto;
+    text-align: center;
+}
+
+.btn-outline-dark {
+    border-radius: 50px;
+}
+</style>
