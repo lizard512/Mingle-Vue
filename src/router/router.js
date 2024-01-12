32a956353cf702fad7fa66cb2fe4df3d442b9cbb
Cindy <onlyyu147@gmail.com>
2024-01-12 14:55:30 +0800

    新增數據分析頁面

--- src/router/router.js
+++ src/router/router.js
@@ -14,6 +14,8 @@ import Chatroom from "@views/user/Chatroom.vue";
 import Order from "@views/user/Order.vue";
 import Account from "@views/user/Account.vue";
 import Review from "@views/user/Review.vue";
+import Analyze from "@views/common/Analyze.vue";
+
 // 2. 定義路由
 const routes = [
   {
@@ -79,6 +81,11 @@ const routes = [
     path: "/review",
     name: "Review",
     component: Review
+  },
+  {
+    path: "/analyze",
+    name: "Analyze",
+    component: Analyze
   }
 ];
 // 3. 建立路由實體並匯出
