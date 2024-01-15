48f93c2d4c7667b2389779bc9cc0ea354def4049
DESKTOP-RAE7E6C\User <>
2024-01-15 17:49:57 +0800

    Fix router paths and remove unused components

---
+++ src/components/register/Register2.vue
@@ -0,0 +1,70 @@
+<template>
+    <p class="title">Email或手機驗證</p>
+    <div class="register-icon">
+        <img src="@images/icon-progress2.png" width="100px">
+    </div>
+    <br>
+    <p style="color: black; font-weight: bold; font-size: 20px;">請輸入驗證碼</p>
+    <span style="color: black;">驗證碼已寄送至</span>
+    <span id="accountID" style="color: black;">0975813702</span>
+    <p style="color: black;">若使用Email驗證但未收到信件，請至垃圾郵件中尋找</p>
+    <div id="otp" class="inputs d-flex flex-row justify-content-center mt-2">
+        <input class="m-2 text-center form-control rounded" type="text" id="first" maxlength="1" />
+        <input class="m-2 text-center form-control rounded" type="text" id="second" maxlength="1" />
+        <input class="m-2 text-center form-control rounded" type="text" id="third" maxlength="1" />
+        <input class="m-2 text-center form-control rounded" type="text" id="fourth" maxlength="1" />
+        <input class="m-2 text-center form-control rounded" type="text" id="fifth" maxlength="1" />
+        <input class="m-2 text-center form-control rounded" type="text" id="sixth" maxlength="1" />
+    </div>
+    <br>
+    <br>
+    <div class="register-btn">
+        <button class="btn btn-lg btn-dark btn-block" type="submit" @click="goToRegister3">下一步:設定密碼</button>
+    </div>
+    <br>
+</template>
+    
+<script setup>
+document.addEventListener("DOMContentLoaded", function (event) {
+    function OTPInput() {
+        const inputs = document.querySelectorAll('#otp > *[id]');
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].addEventListener('keydown', function (event) {
+                if (event.key === "Backspace") {
+                    inputs[i].value = ''; if (i !== 0) inputs[i - 1].focus();
+                } else {
+                    if (i === inputs.length - 1 && inputs[i].value !== '') {
+                        return true;
+                    } else if (event.keyCode > 47 && event.keyCode < 58) {
+                        inputs[i].value = event.key; if (i !== inputs.length - 1) inputs[i + 1].focus(); event.preventDefault();
+                    } else if (event.keyCode > 64 && event.keyCode < 91) {
+                        inputs[i].value = String.fromCharCode(event.keyCode); if (i !== inputs.length - 1) inputs[i + 1].focus(); event.preventDefault();
+                    }
+                }
+            });
+        }
+    } OTPInput();
+});
+
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const goToRegister3 = () => {
+    router.push('/register/register3')
+}
+</script>
+
+<style src="@styles/register.css" scoped></style>
+<style scoped>
+.inputs input {
+    width: 40px;
+    height: 40px;
+}
+
+input[type=number]::-webkit-inner-spin-button,
+input[type=number]::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    margin: 0
+}
+</style>
---
+++ src/components/register/Register3.vue
@@ -0,0 +1,65 @@
+<template>
+    <p class="title">設定登入密碼</p>
+    <div class="register-icon">
+        <img src="@images/icon-progress3.png" width="100px">
+    </div>
+    <br>
+    <p style="text-align: center; color: black; font-weight: bold; font-size: 20px; margin-top: -20px;">您的帳號為
+    </p>
+    <div class="account-icon">
+        <img src="@images/icon-account2.png" width="50px">
+    </div>
+    <p id="accountID" style="text-align: center;">0975813702</p>
+    <br>
+    <div class="passwordDiv">
+        <input type="password" class="password" name="password" placeholder="請輸入密碼" required>
+    </div>
+    <br>
+    <div class="passwordDiv">
+        <input type="password" class="password" name="password" placeholder="確認密碼" required>
+    </div>
+    <p style="color: red;">•密碼長度為 8 - 50 個字元</p>
+    <p>•請使用英文字母、數字與符號的半形字元</p>
+    <p>•密碼前後不可空格</p>
+    <p>•密碼與確認密碼必須相同</p>
+    <br>
+    <div class="register-btn">
+        <button class="btn btn-lg btn-dark btn-block" type="submit">設定完成</button>
+    </div>
+    <br>
+</template>
+    
+<script setup>
+</script>
+
+
+<style src="@styles/register.css" scoped></style>
+<style scoped>
+
+.account-icon {
+    text-align: center;
+    margin-top: -10px;
+}
+
+.accountDiv {
+    text-align: center;
+}
+
+.passwordDiv {
+    text-align: center;
+}
+
+
+.account {
+    overflow: hidden;
+    width: 100%;
+}
+
+.password {
+    overflow: hidden;
+    width: 100%;
+}
+
+
+
+</style>
