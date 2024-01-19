0963b8a7ec8adf916b501be3ae9204470ad251b3
Cindy <onlyyu147@gmail.com>
2024-01-19 17:18:33 +0800

    modify order 頁面
    order1:
    -完成日期篩選功能
    -完成送出資料到下一頁
    order2:
    -完成大致排版

--- src/components/order/Order2.vue
+++ src/components/order/Order2.vue
@@ -21,14 +21,10 @@
 
                                     <div class="d-flex justify-content-between align-items-center mb-5">
                                         <div>
-                                            <h5 class="mb-0">訂單編號 <span
+                                            <h5 class="mb-0">訂單編號: <span
                                                     class="text-primary font-weight-bold">ID1234567</span></h5>
-                                        </div>
-
-                                        <div class="text-end">
-                                            <p class="mb-0">訂購日期:
-                                            <p>{{ dataForOrder2.startDate }}</p>
-                                            </p>
+                                            <p class="mb-0 text-muted" rderDate="orderDate">訂購日期:{{ dataForOrder2.orderDate
+                                            }} </p>
                                         </div>
 
                                     </div>
@@ -83,132 +79,136 @@
 
                     <!--  card start  -->
 
-                    <div class="col-12 order-md-last">
+                    <div class="row">
 
 
                         <!-- 打工預定 card start-->
+                        <div class="mb-4 col-md-4">
+                            <h4 class="d-flex justify-content-between align-items-center mb-3">
+                                <span class="text-primary">打工預定</span>
+                            </h4>
 
-                        <h4 class="d-flex justify-content-between align-items-center mb-3">
-                            <span class="text-primary">打工預定</span>
-                        </h4>
+                            <ul class="list-group col-12 ">
+                                <li class="list-group-item d-flex justify-content-between lh-sm">
+                                    <div>
+                                        <h6 class="my-0">打工類型</h6>
+                                        <small class="text-muted"> work type</small>
+                                    </div>
+                                    <span class="primary">{{ workdetail.worktype }}</span>
+                                </li>
 
-                        <ul class="list-group mb-3">
-                            <li class="list-group-item d-flex justify-content-between lh-sm">
-                                <div>
-                                    <h6 class="my-0">打工類型</h6>
-                                    <small class="text-muted"> </small>
-                                </div>
-                                <span class="text-muted">{{ dataForOrder2.username }}</span>
-                            </li>
+                                <li class="list-group-item d-flex justify-content-between lh-sm">
+                                    <div>
+                                        <h6 class="my-0">打工項目</h6>
+                                        <small class="text-muted">work name</small>
+                                    </div>
+                                    <span class="primary">{{ workdetail.name }}</span>
+                                </li>
+                                <li class="list-group-item d-flex justify-content-between lh-sm">
+                                    <div>
+                                        <h6 class="my-0">打工期間</h6>
+                                        <small class="text-muted">work period</small>
+                                    </div>
+                                    <span class="primary">{{ dataForOrder2.selectedPeriod }}天</span>
+                                </li>
 
-                            <li class="list-group-item d-flex justify-content-between lh-sm">
-                                <div>
-                                    <h6 class="my-0">打工項目</h6>
-                                    <small class="text-muted">ddd</small>
-                                </div>
-                                <span class="text-muted">照護</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between lh-sm">
-                                <div>
-                                    <h6 class="my-0">打工期間</h6>
-                                    <small class="text-muted">work period</small>
-                                </div>
-                                <span class="text-muted">7天</span>
-                            </li>
+                                <li class="list-group-item d-flex justify-content-between lh-sm">
+                                    <div>
+                                        <h6 class="my-0">打工人數</h6>
+                                        <small class="text-muted">order applicants</small>
+                                    </div>
+                                    <span class="primary">{{ dataForOrder2.selectedAccomodator }}人</span>
+                                </li>
 
-                            <li class="list-group-item d-flex justify-content-between lh-sm">
-                                <div>
-                                    <h6 class="my-0">打工人數</h6>
-                                    <small class="text-muted">order applicants</small>
-                                </div>
-                                <span class="text-muted">1人</span>
-                            </li>
-
-                            <li class="list-group-item d-flex justify-content-between lh-sm">
-                                <div>
-                                    <h6 class="my-0">開始日期</h6>
-                                    <small class="text-muted">order startDate</small>
-                                </div>
-                                <span class="text-muted">2023/11/20</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between lh-sm">
-                                <div>
-                                    <h6 class="my-0">結束日期</h6>
-                                    <small class="text-muted">order endDate</small>
-                                </div>
-                                <span class="text-muted">2023/11/27</span>
-                            </li>
-                        </ul>
-                        <!-- 打工預定 card end-->
-
+                                <li class="list-group-item d-flex justify-content-between lh-sm">
+                                    <div>
+                                        <h6 class="my-0">開始日期</h6>
+                                        <small class="text-muted">order startDate</small>
+                                    </div>
+                                    <span class="primary">{{ dataForOrder2.startDate }}</span>
+                                </li>
+                                <li class="list-group-item d-flex justify-content-between lh-sm">
+                                    <div>
+                                        <h6 class="my-0">結束日期</h6>
+                                        <small class="text-muted">order endDate</small>
+                                    </div>
+                                    <span class="primary">{{ dataForOrder2.endDate }}</span>
+                                </li>
+                            </ul>
+                            <!-- 打工預定 card end-->
+                        </div>
 
 
 
                         <!-- 房源預定 Card start -->
-                        <h4 class="d-flex justify-content-between align-items-center mb-3">
-                            <span class="text-primary">住宿預定</span>
-                        </h4>
+                        <div class="mb-4 col-md-4">
+                            <h4 class="d-flex justify-content-between align-items-center mb-3">
+                                <span class="text-primary">住宿預定</span>
+                            </h4>
 
-                        <ul class="list-group mb-3">
-                            <li class="list-group-item d-flex justify-content-between lh-sm">
-                                <div>
-                                    <h6 class="my-0">房型</h6>
-                                    <small class="text-muted">house type</small>
-                                </div>
-                                <span class="text-muted">雙人房</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between lh-sm">
-                                <div>
-                                    <h6 class="my-0">房間</h6>
-                                    <small class="text-muted">house name</small>
-                                </div>
-                                <span class="text-muted">301</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between lh-sm">
-                                <div>
-                                    <h6 class="my-0">人數</h6>
-                                    <small class="text-muted">numbers</small>
-                                </div>
-                                <span class="text-muted">1人</span>
-                            </li>
-                        </ul>
-                        <!-- 房源預定 Card end -->
+                            <ul class="list-group col-12 ">
+                                <li class="list-group-item d-flex justify-content-between lh-sm">
+                                    <div>
+                                        <h6 class="my-0">房型</h6>
+                                        <small class="text-muted">house type</small>
+                                    </div>
+                                    <span class="text-muted">雙人房</span>
+                                </li>
+                                <li class="list-group-item d-flex justify-content-between lh-sm">
+                                    <div>
+                                        <h6 class="my-0">房間</h6>
+                                        <small class="text-muted">house name</small>
+                                    </div>
+                                    <span class="text-muted">301</span>
+                                </li>
+                                <li class="list-group-item d-flex justify-content-between lh-sm">
+                                    <div>
+                                        <h6 class="my-0">人數</h6>
+                                        <small class="text-muted">numbers</small>
+                                    </div>
+                                    <span class="text-muted">{{ dataForOrder2.selectedAccomodator }}人</span>
+                                </li>
+                            </ul>
+                            <!-- 房源預定 Card end -->
+                        </div>
 
-                        <!-- 媒合費用 Card start-->
-                        <h4 class="d-flex justify-content-between align-items-center mb-3">
-                            <span class="text-primary">總計費用</span>
-                        </h4>
-                        <ul class="list-group mb-3">
-                            <li class="list-group-item d-flex justify-content-between lh-sm">
-                                <div>
-                                    <h6 class="my-0">打工人數</h6>
-                                    <small class="text-muted">order applicants</small>
-                                </div>
-                                <span class="text-muted">2023/11/20</span>
-                            </li>
+                        <div class="mb-4 col-md-4">
 
-                            <li class="list-group-item d-flex justify-content-between bg-light">
-                                <div>
-                                    <h6 class="my-0">媒合費用</h6>
-                                    <small class="text-muted">service price</small>
-                                </div>
-                                <strong>$300<span>(TWD)</span></strong>
-                            </li>
-                        </ul>
-                        <!-- 媒合費用 Card end-->
+                            <!-- 媒合費用 Card start-->
+                            <h4 class="d-flex justify-content-between align-items-center mb-3">
+                                <span class="text-primary">總計費用</span>
+                            </h4>
+                            <ul class="list-group mb-3 col-12 ">
+                                <li class="list-group-item d-flex justify-content-between lh-sm">
+                                    <div>
+                                        <h6 class="my-0">打工人數</h6>
+                                        <small class="text-muted">order applicants</small>
+                                    </div>
+                                    <span class="text-muted">{{ dataForOrder2.selectedAccomodator }}人</span>
+                                </li>
 
+                                <li class="list-group-item d-flex justify-content-between bg-light">
+                                    <div>
+                                        <h6 class="my-0">媒合費用</h6>
+                                        <small class="text-muted">service price</small>
+                                    </div>
+                                    <strong>{{ dataForOrder2.selectedAccomodator * 300 }}元<span>(TWD)</span></strong>
+                                </li>
+                            </ul>
+                            <!-- 媒合費用 Card end-->
 
+                        </div>
 
                         <!-- button start -->
-                        <hr class="my-4">
+                        <hr class="my-4 col-md-12">
                         <button class="w-50 btn change btn-primary btn-lg" type="submit" @click="goToOrder1()">修改資料</button>
                         <button class="w-50 btn btn-secondary btn-lg" type="submit" @click="goToOrder3()">下一步</button>
                         <!-- button end -->
 
+
+
+                        <!--  right card end  -->
                     </div>
-
-                    <!--  right card end  -->
-
                 </form>
             </div>
             <!-- 預定住宿 end-->
@@ -222,9 +222,11 @@
     
 <script setup>
 
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, reactive } from 'vue';
 
 import { useRouter } from 'vue-router';
+
+import axios from 'axios';
 
 const router = useRouter();
 
@@ -240,17 +242,33 @@ const loaddata = () => {
     // 从 Pinia store 获取数据
     dataForOrder2.value = orderStore.orderData;
     console.log('Received dataForOrder2:', dataForOrder2);
+    workid.value = dataForOrder2.value.workid
+    console.log(workid.value)
 
 
 }
 
 
-onMounted(() => {
+//============查詢工作資料============
 
+
+const workdetail = reactive({})
+const workid = ref('');
+
+
+const loadworkDetail = async () => {
+    const Work_API_URL = 'http://localhost:8080/order/' + workid.value;
+    const response = await axios.post(Work_API_URL);
+    Object.assign(workdetail, response.data);
+    console.log('Received workdetail:', workdetail);
+}
+
+
+onMounted(async () => {
     loaddata();
-
-
+    await loadworkDetail();
 });
+
 
 
 
