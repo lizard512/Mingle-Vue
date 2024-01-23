06548d3864d098007662a583156bec0f5a62175a
aura70001 <b0232036@ems.niu.edu.tw>
2024-01-23 12:05:48 +0800

    update chatroom

--- src/views/user/Chatroom.vue
+++ src/views/user/Chatroom.vue
@@ -7,11 +7,17 @@
             </div>
             <div>
                 <div class="ms-5 me-4 list-group" v-for="item in roomList"
-                    @click="selectRoom(item.senderid, item.recieverid)">
-                    <button type="button" class="list-group-item list-group-item-action">
-                        <img class="rounded-circle float-start me-4" src="https://picsum.photos/50/50">
-                        <strong>{{ item.recievername }}</strong>
-                        <small>{{ item.date }}</small>
+                    @click="selectRoom(item.senderid, item.recieverid, item.sendername, item.recievername)">
+                    <button type="button" class="list-group-item list-group-item-action list-group-item-light">
+                        <img class="mt-1 ms-1 me-4 rounded-circle float-start" src="https://picsum.photos/50/50">
+                        <div class="mt-1 d-flex justify-content-between align-items-center">
+                            <div>
+                                <strong>{{ item.recievername }}</strong>
+                            </div>
+                            <div class="text-end">
+                                <small class="text-dark">{{ item.date }} {{ item.time }}</small>
+                            </div>
+                        </div>
                         <p class="text-truncate">{{ item.contents }}</p>
                     </button>
                 </div>
@@ -20,19 +26,23 @@
         </div>
         <!-- Right => chat-name, chat-container, input-group -->
         <div class="col-md-8 mh-100">
-            <div class="bg-light chat-name">
-                <strong>{{ selectedUserID }}</strong>
+            <div class="p-2 bg-light chat-name">
+                <strong class="ms-2 animate__animated animate__fadeIn"> <i class="me-1 fa-solid fa-house-chimney-user"></i>
+                    {{
+                        selectedUserName }}</strong>
             </div>
-
             <div class="chat-container border overflow-auto d-flex flex-column p-3" ref="chatContainer">
+                <div class="d-flex justify-content-center align-items-center flex-grow-1" v-if="beginChat">
+                    <h1 class="text-dark"> 請點擊左側聊天室列表開始聊天 <i class="fa-solid fa-comment-sms"></i></h1>
+                </div>
                 <template v-for="item in messages">
                     <div v-if="senderID !== item.senderID"
-                        class="px-3 py-2 m-1 rounded-3 flex-start align-self-start chat-message chat-received">
+                        class="px-3 py-2 m-1 rounded-3 flex-start align-self-start chat-message chat-received animate__animated animate__fadeIn">
                         <p>{{ item.contents }}
                         </p>
                     </div>
                     <div v-if="senderID === item.senderID"
-                        class="px-3 py-2 m-1 rounded-3 align-self-end chat-message chat-sent">
+                        class="px-3 py-2 m-1 rounded-3 align-self-end chat-message chat-sent animate__animated animate__fadeIn">
                         <p>{{ item.contents }}</p>
                     </div>
                 </template>
@@ -53,7 +63,8 @@
 </template>
     
 <script setup>
-let global = globalThis;
+let global = globalThis; // 因應stomp及sockjs-client所設
+
 import { ref, onMounted } from 'vue';
 
 // Stomp & Sockjs-client
@@ -66,42 +77,42 @@ const path = 'http://localhost:8080'
 import axios from 'axios';
 import VueCookies from 'vue-cookies';
 import Swal from 'sweetalert2'
-const roomList = ref(null);
-const chatContainer = ref(null);
-const inputField = ref(null);
-const messages = ref([]);
-const contents = ref('');
-const senderID = ref('');
-const selectedUserID = ref('');
+const roomList = ref(null);         // 左側列表，初始、收發訊息時渲染。
+const chatContainer = ref(null);    // 右側訊息，初始、收發訊息、點擊聊天列表時渲染。
+const contents = ref('');           // 左側資料
+const messages = ref([]);           // 右側資料
+const senderID = ref('');           // 本使用者id，初始時賦值，之後永不變更。
+const selectedUserID = ref('');     // 對方使用者id，點擊聊天列表更新。
+const selectedUserName = ref('');   // 對方使用者姓名，點擊聊天列表更新。
+const inputField = ref(null);       // 訊息輸入欄位，enter可觸發。
+const beginChat = ref(true);        // 初始畫面，提醒點擊。
 
-
+// 賦值→連線→渲染(左側)聊天列表→
 onMounted(async () => {
     await initAssign();
     initConnect();
     await findAllChat();
     // await findAllMessages();
     messageEnd();
-    inputField.value.focus();
 });
-// 初始賦值，之後改寫
-async function initAssign() {
-    senderID.value = getuserid();
-}
-const getuserid =
-    () => {
-        const sessionToken = VueCookies.get('sessionToken');
-        const userid = String(sessionToken).substring(32, sessionToken.length);
-        return userid
-    }
 
+// 初始賦值(確定使用者)
+async function initAssign() {
+    const sessionToken = VueCookies.get('sessionToken');
+    senderID.value = String(sessionToken).substring(32, sessionToken.length);
+}
+
+// stompjs連線
 function initConnect() {
     stompClient.connect({}, onConnected, onError);
 }
+
+// 訂閱
 function onConnected() {
     stompClient.subscribe(`/user/${senderID.value}/queue/messages`, onMessageReceived);
     stompClient.subscribe(`/user/public`, onMessageReceived);
-    console.log('stompClient', stompClient);
-    console.log('socket', socket);
+    // console.log('stompClient', stompClient);
+    // console.log('socket', socket);
 
     // register the connected user
     // stompClient.send("/app/user.addUser",
@@ -111,6 +122,8 @@ function onConnected() {
     // document.querySelector('#connected-user-fullname').textContent = fullname;
     // findAndDisplayConnectedUsers().then();
 }
+
+// 連線中斷
 function onError() {
     Swal.fire({
         icon: 'error',
@@ -120,6 +133,7 @@ function onError() {
 
     })
 }
+
 // 滾動到最底、清空對話欄位、焦點設置回欄位
 function messageEnd() {
     if (chatContainer.value) {
@@ -128,6 +142,7 @@ function messageEnd() {
     contents.value = ''
     inputField.value.focus();
 }
+// 傳
 async function sendMessage(event) {
     const trimmedContents = contents.value.trim();
     if (trimmedContents !== '' && stompClient) {
@@ -148,6 +163,7 @@ async function sendMessage(event) {
     // await findAllMessages();
     // messageEnd();
 }
+// 收
 async function onMessageReceived(payload) {
     // await findAndDisplayConnectedUsers();
     console.log('Message received', payload);
@@ -184,24 +200,28 @@ async function findAllMessages() {
 
         });
 }
-// 聊天室渲染
+// 聊天列表渲染
 async function findAllChat() {
     await axios.get(`${path}/messages/${senderID.value}/preview`)
         .then(function (response) {
             response.data.forEach(function (item) {
-
+                /* 
+                最後一筆訊息可能是自己送出，也可能是對方送出。
+                由於聊天室永遠顯示對方名字，因此畫面渲染前，須將收信者名字固定為對方。
+                */
                 if (item.recieverid == senderID.value) {
                     item.recievername = item.sendername;
                 }
 
+                // 日期整理
                 item.createdTime = new Date(item.createdTime);
                 const year = item.createdTime.getFullYear();
-                const month = item.createdTime.getMonth() + 1;
-                const date = item.createdTime.getDate();
+                const month = ('0' + (item.createdTime.getMonth() + 1)).slice(-2);
+                const date = ('0' + item.createdTime.getDate()).slice(-2);
                 item.date = `${year}/${month}/${date}`;
-
+                // 時間整理
                 const hour = item.createdTime.getHours();
-                const minutes = item.createdTime.getMinutes();
+                const minutes = ('0' + item.createdTime.getMinutes()).slice(-2);
                 item.time = `${hour}:${minutes}`;
             });
             roomList.value = response.data;
@@ -212,18 +232,21 @@ async function findAllChat() {
 }
 
 // 選聊天室
-async function selectRoom(userid, recieverid) {
-    await assignSelectedUser(userid, recieverid);
+async function selectRoom(senderid, recieverid, sendername, recievername) {
+    beginChat.value = false;        // 關掉初始提醒
+    await assignSelectedUser(senderid, recieverid, sendername, recievername);
     await findAllMessages();
     messageEnd();
 }
 
 // 更改selectedID
-async function assignSelectedUser(userid, recieverid) {
-    if (senderID.value == userid) {
+async function assignSelectedUser(senderid, recieverid, sendername, recievername) {
+    if (senderID.value == senderid) {
         selectedUserID.value = recieverid;
+        selectedUserName.value = recievername;
     } else {
-        selectedUserID.value = userid;
+        selectedUserID.value = senderid;
+        selectedUserName.value = sendername;
     }
 }
 
