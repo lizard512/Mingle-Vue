379bddfcf114addc66b95e9c28d8f682bd2b212d
LiTseLin <lizard@smail.nchu.edu.tw>
2024-01-24 21:57:30 +0800

    Refactor WorkSearch.vue component by removing unused code and adding city filter

--- src/views/common/WorkSearch.vue
+++ src/views/common/WorkSearch.vue
@@ -9,16 +9,6 @@
                             <div class="col-md-4">
                                 <input type="text" class="form-control border-0 py-3" placeholder="用關鍵字查詢">
                             </div>
-                            <div class="col-md-4">
-                                <select class="form-select border-0 py-3">
-                                    <option selected>打工地區</option>
-                                    <option value="1">北部區域</option>
-                                    <option value="2">西部區域</option>
-                                    <option value="3">南部區域</option>
-                                    <option value="4">東部區域</option>
-                                    <option value="5">福建省</option>
-                                </select>
-                            </div>
                         </div>
                     </div>
                     <div class="col-md-2">
@@ -33,14 +23,25 @@
         <div class="container-fluid">
             <!--Search Header Start-->
             <div class="row g-4 align-items-center py-3 bg-light " :class="{ 'sticky-header': isSticky }">
-                <div class="col-lg-4">
+                <div class="col-lg-3">
                     <div class="text-start mx-auto wow animate__animated animate__slideInLeft inline-flex"
                         data-wow-delay="0.1s">
                         <h1 class="me-3">打工機會</h1>
                         <p>快來查看正在徵求幫助者的項目！</p>
                     </div>
                 </div>
-                <div class="col-lg-4 mx-auto">
+                <div class="col-lg-3 mx-auto">
+                    <select v-model="filters.city[0]" @change="reloadWork()">
+                        <option value="">所有縣市</option>
+                        <optgroup v-for="(group, area) in groupedCities" :label="area" :key="area">
+                            <option v-for="city in group" :value="city.city" :key="city.city">
+                                {{ city.city }}
+                            </option>
+                            <option :value="area">所有{{ area.substring(0,2) }}縣市</option>
+                        </optgroup>
+                    </select>
+                </div>
+                <div class="col-lg-3 mx-auto">
                     <ul class="nav d-inline-flex justify-content-end ">
                         <li class="nav-item me-2" v-for="worktypeID in worktypeIDs" :key="worktypeID.worktypeID">
                             <a class="btn btn-outline-dark"
@@ -51,7 +52,7 @@
                         </li>
                     </ul>
                 </div>
-                <div class="col-lg-4 text-end wow animate__animated animate__slideInRight" data-wow-delay="0.1s">
+                <div class="col-lg-3 text-end wow animate__animated animate__slideInRight" data-wow-delay="0.1s">
                     <ul class="nav nav-pills d-inline-flex justify-content-end">
                         <li class="nav-item me-2">
                             <a class="btn btn-outline-primary active" data-bs-toggle="pill"
@@ -96,9 +97,8 @@
                             </div>
                             <div class="d-flex border-top">
                                 <small class="flex-fill text-center border-end py-2"><i
-                                        class="fa fa-calendar text-primary me-2"></i>{{ formatDate(work.startDate)
-                                        }} ~
-                                    {{ formatDate(work.endDate) }}</small>
+                                        class="fa fa-calendar text-primary me-2"></i>{{ work.startDate.toString().substring(0, 10) }} ~
+                                        {{ work.endDate.toString().substring(0, 10) }}</small>
                             </div>
                             <div class="d-flex border-top">
                                 <small class="flex-fill text-center border-end py-2"><i
@@ -129,62 +129,81 @@
 </template>
     
 <script setup>
-// 引用函示庫
-import { ref, onMounted, onUnmounted, toRaw } from 'vue';
+//// 引用函示庫
+import { ref, computed, onMounted, onUnmounted, toRaw } from 'vue';
 import axios from 'axios';
 
-// 接收資料
+//// 接收資料庫資料
 let works = ref([]);
 const worktypeIDs = ref([]);
-const cityIDs = ref([]);
+const cities = ref([]);
 const baseURL = "http://localhost:8080";
 
-// 工作資料載入用的預設參數
-let currentPage = ref(0);
-let direction = 'DESC'; // 排序方向
-let property = 'views'; // 排序屬性
+//// 預設參數
+// 載入相關
+let currentPage = ref(0); // 當前頁數
 const size = 6; // 每次載入的數量
 const isLoading = ref(false); //避免重複載入
 let isEnd = ref(false);
+// 排序相關
+let direction = 'DESC'; // 排序方向
+let property = 'views'; // 排序屬性
 let isArrowUp = ref(true);// 排序按紐的箭頭方向
-const isSticky = ref(false); // Sticky Header
-let selectedWorkType = ref(null); // 選擇的工作類型
+// 篩選相關
+const areaOrder = ['北部區域', '中部區域', '南部區域', '東部區域', '福建省'];
 let filters = ref({
     worktype: [],
-    // 其他過濾條件...
+    city: [''],
+    // 其他篩選條件...
 });
 
+// 其他
+const isSticky = ref(false); // Sticky Header
 
-// 生命週期
+
+//// 生命週期
 onMounted(async () => {
     await loadWork();
-    await loadWorkType();
+    await loadWorktype();
     await loadCity();
     window.addEventListener('scroll', infiniteScroll);
     window.addEventListener('scroll', checkSticky);
     // document.body.classList.add('no-scroll');
 });
-
 onUnmounted(() => {
     window.removeEventListener('scroll', infiniteScroll);
     window.removeEventListener('scroll', checkSticky);
     // document.body.classList.remove('no-scroll');
 });
 
+//// 方法
 // 載入工作分類
-const loadWorkType = async () => {
+const loadWorktype = async () => {
     const response = await axios.get(baseURL + '/api/worktype/getWorktypes');
     worktypeIDs.value = response.data;
 }
-
 // 載入城市
 const loadCity = async () => {
     const response = await axios.get(baseURL + '/api/city/getCities');
-    cityIDs.value = response.data;
-    console.log(cityIDs.value);
+    cities.value = response.data;
 }
+// 將城市依area分組、排序
+const groupedCities = computed(() => {
+    const groups = cities.value.reduce((groups, city) => {
+        if (!groups[city.area]) {
+            groups[city.area] = [];
+        }
+        groups[city.area].push(city);
+        return groups;
+    }, {});
 
-// 載入第一頁的工作資料(預設為熱門排序)
+    return Object.keys(groups).sort((a, b) => areaOrder.indexOf(a) - areaOrder.indexOf(b)).reduce((sortedGroups, key) => {
+        sortedGroups[key] = groups[key];
+        return sortedGroups;
+    }, {});
+});
+
+// 載入一頁工作列表
 const loadWork = async () => {
     if (isEnd.value || isLoading.value) return;
     isLoading.value = true;
@@ -216,6 +235,7 @@ const loadWork = async () => {
     }
 };
 
+// 重新載入工作列表
 const reloadWork = async () => {
     works.value = []; // 清空工作列表
     currentPage.value = 0; // 重設頁數
@@ -225,6 +245,7 @@ const reloadWork = async () => {
     loadWork();
 }
 
+// 無限卷軸功能
 const infiniteScroll = () => {
     if (isLoading.value) return;
     if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 200) {
@@ -232,7 +253,7 @@ const infiniteScroll = () => {
     }
 };
 
-
+// 篩選器: 依工作類型
 const toggleWorkType = (worktypeID) => {
     const index = filters.value.worktype.indexOf(worktypeID);
     if (index >= 0) {
@@ -241,12 +262,11 @@ const toggleWorkType = (worktypeID) => {
     } else {
         filters.value.worktype.push(worktypeID);
     }
-    console.log(toRaw(filters.value));
 
     reloadWork(); // 重新載入工作列表
 };
 
-// 依照熱門、最新、屆期更改工作排序
+// 排序: 熱門、最新、即將截止
 const toggleSorts = (propertyParam, directionParam) => {
     property = propertyParam; // 更新排序欄位
     direction = directionParam; // 更新排序方向
@@ -254,7 +274,7 @@ const toggleSorts = (propertyParam, directionParam) => {
     reloadWork();
 }
 
-// 依照參加人數排序工作
+// 排序: 參加人數升冪降冪
 const toggleSortByAttendance = () => {
     isArrowUp.value = !isArrowUp.value;
 
@@ -271,10 +291,10 @@ const formatDate = (dateString) => {
     return `${date.getFullYear()}/${("0" + (date.getMonth() + 1)).slice(-2)}/${("0" + date.getDate()).slice(-2)}`;
 }
 
+// Sticky Header
 const checkSticky = () => {
     isSticky.value = window.scrollY > 45;
 };
-
 
 </script>
 
