a56673d42d1b17fe0dd1387e5cd7eb44e46755e1
DESKTOP-RAE7E6C\User <spi2013416@gmail.com>
2024-01-24 13:00:04 +0800

    A好

--- src/components/AccountLeftNavBar.vue
+++ src/components/AccountLeftNavBar.vue
@@ -2,22 +2,18 @@
     <div class="col-2 md-left bg-primary" style="float: left;font-size:x-large; text-align: center;">
         <br><br>
         <h4>我的帳戶</h4><br><br>
-<<<<<<< HEAD
-        <div class="fa bs-secondary" @click="clickHandler(isShow = 1)"> <a href="#">基本資料</a></div><br><br>
-        <div class="fa" @click="clickHandler(isShow = 4)"> <a href="#">個人介紹</a></div><br><br>
-        <div class="fa"> <a href="#">住宿/打工預定</a></div><br><br>
-        <div class="fa" @click="clickHandler(isShow = 2)"> <a href="#">旅行足跡</a></div><br><br>
-        <div class="fa" @click="clickHandler(isShow = 3)"> <a href="#">心願清單</a></div><br><br>
-=======
-        <div class="fa" @click="clickHandler(isShow = 1)"> <a href="#" style="color:red;">基本資料</a></div><br><br>
-        <div class="fa" @click="clickHandler(isShow = 4)"> <a href="#" style="color:red;">個人介紹</a></div><br><br>
-        <div class="fa"> <a href="#" style="color:red;">住宿/打工預定</a></div><br><br>
-        <div class="fa fa-solid fa-shoe-prints" @click="clickHandler(isShow = 2)"> <a href="#" style="color:red;">旅行足跡</a></div><br><br>
-        <div class="fa" @click="clickHandler(isShow = 3)"> <a href="#" style="color:red;">心願清單</a></div><br><br>
->>>>>>> 546300b7870482061f22c47446a35f13072e6f31
+        <div class="fa fa-solid fa-user" @click="clickHandler(isShow = 1)"> <a href="#" class="fa"><font-awesome-icon
+                    icon="fa-solid fa-user-secret" />基本資料</a></div><br><br>
+        <div class="fa fa-solid fa-pen" @click="clickHandler(isShow = 4)"> <a href="#" class="fa">個人介紹</a></div>
+        <br><br>
+        <div class="fa fa-brands fa-phoenix-framework"> <a href="#" class="fa">住宿/打工預定</a></div><br><br>
+        <div class="fa fa-solid fa-shoe-prints" @click="clickHandler(isShow = 2)"> <a href="#" class="fa">旅行足跡</a>
+        </div><br><br>
+        <div class="fa fa-brands fa-gratipay" @click="clickHandler(isShow = 3)"> <a href="#" class="fa">心願清單</a></div>
+        <br><br>
     </div>
 </template>
-    
+
 <script setup >
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
@@ -29,4 +25,9 @@ const clickHandler = showVeiw => {
 }
 </script>
     
-<style></style>
+<style>
+.fa {
+    color: tomato;
+    color: chocolate;
+}
+</style>
