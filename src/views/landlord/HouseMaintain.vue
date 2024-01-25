877ec6cd9add09b5945a1efbb00d7e1c6cd8a6fb
gitInterface <97436826+gitInterface@users.noreply.github.com>
2024-01-25 09:17:30 +0800

    modify HouseMaintain.vue

--- src/views/landlord/HouseMaintain.vue
+++ src/views/landlord/HouseMaintain.vue
@@ -1,342 +1,193 @@
-<template id="app">
-    <div id="select" class="tableDiv" v-show="isShowSelect">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>Price</th>
-                    <th>Make</th>
-                    <th>Expire</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="item in foundItems" :key="item.id">
-                    <td>{{ item.id }}</td>
-                    <td>{{ item.name }}</td>
-                    <td>{{ item.price }}</td>
-                    <td>{{ item.make }}</td>
-                    <td>{{ item.expire }}</td>
-                    <td>
-                        <button type="button" @click="callFindById(item.id)">修改</button>
-                        <button type="button" @click="callRemove(item.id)">刪除</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+<template>
+  <br>
+  <div class="tableDiv" id="app">
+    <h3>房源表</h3>
+    <table class="table">
+      <thead class="table-dark">
+        <tr>
+          <th>House ID</th>
+          <th>Lord ID</th>
+          <th>更新</th>
+          <th>房源類型</th>
+          <th>縣市</th>
+          <th>名稱</th>
+          <th>描述</th>
+          <th>地址</th>
+          <th>郵遞區號</th>
+          <th>床位</th>
+          <th>狀態</th>
+          <th>備註</th>
+          <th>有Wifi</th>
+          <th>有TV</th>
+          <th>有廚房</th>
+          <th>有洗衣機</th>
+          <th>有停車位</th>
+          <th>有冷氣</th>
+          <th>有私人空間</th>
+          <th>有游泳池</th>
+          <th>有健身房</th>
+          <th>新增時間</th>
+          <th>更新時間</th>
+          <th>是否刪除</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="house in mappedHouses" :key="house.houseid">
+          <td>{{ house.houseid }}</td>
+          <td>{{ house.lordid }}</td>
+          <td>
+            <button type="button" class="btn btn-light update" @click="openUpdateModal(house.houseid)">修改</button>
+            <button type="button" class="btn btn-danger delete" @click="confirmDelete(house.houseid)">刪除</button>
+          </td>
+          <td>{{ house.houseType }}</td>
+          <td>{{ house.city }}</td>
+          <td>{{ house.name }}</td>
+          <td>{{ house.description }}</td>
+          <td>{{ house.address }}</td>
+          <td>{{ house.postCode }}</td>
+          <td>{{ house.beds }}</td>
+          <td>{{ house.status }}</td>
+          <td>{{ house.notes }}</td>
+          <td>{{ house.hasWifi }}</td>
+          <td>{{ house.hasTV }}</td>
+          <td>{{ house.hasKitchen }}</td>
+          <td>{{ house.hasLaundry }}</td>
+          <td>{{ house.hasParkingLot }}</td>
+          <td>{{ house.hasAirconditioner }}</td>
+          <td>{{ house.hasPersonalSpace }}</td>
+          <td>{{ house.hasPool }}</td>
+          <td>{{ house.hasGym }}</td>
+          <td>{{ house.createdAt }}</td>
+          <td>{{ house.updatedAt }}</td>
+          <td>{{ house.isDeleted }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <!-- modal is here -->
+    <div class="modal" tabindex="-1" role="dialog" v-show="isUpdateModalVisible">
+      <div class="modal-dialog" role="document">
+        <!-- Modal content -->
+        <div class="modal-content">
+          <span class="close" @click="closeUpdateModal">&times;</span>
+          <!-- Update form or content here -->
+          <h2>Update House</h2>
+          <!-- Add your form fields for updating -->
+          <p>Updating House ID: {{ currentHouseToUpdate }}</p>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
-const app = Vue.createApp({
-    data: function () {
-        return {
-            //畫面控制 start
-            isShowSelect: true,
-            isShowModify: true,
-            isShowButtonInsert: true,
-            isShowButtonUpdate: true,
-            isShowTableFooter: true,
-            //畫面控制 end
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { ref, onMounted, computed } from 'vue';
+import Swal from 'sweetalert2';
+import axios from 'axios';
 
-            //資料 start
-            findId: null,
-            findName: null,
-            id: null,
-            name: null,
-            price: null,
-            make: null,
-            expire: null,
-            foundItems: [],
-            //資料 end
-        }
-    },
-    methods: {
-        showSelect: function () {
-            this.isShowSelect = true;
-            this.isShowModify = false;
+const houses = ref([]);
+const isUpdateModalVisible = ref(false);
+let currentHouseToUpdate = null;
 
-            this.findId = null;
-            this.findName = null;
-        },
-        showModify: function (action) {
-            this.isShowSelect = false;
-            this.isShowModify = true;
+const fetchHouses = () => {
+  fetch('http://localhost:8080/api/house/findAll')
+    .then((response) => response.json())
+    .then((data) => {
+      houses.value = data;
+    })
+    .catch((error) => {
+      console.error('Error fetching houses:', error);
+    });
+};
 
-            this.id = null;
-            this.name = null;
-            this.price = null;
-            this.make = null;
-            this.expire = null;
+const mapBoolToString = (value) => {
+  return +value ? '是' : '否';
+};
 
-            if (action === "insert") {
-                this.isShowButtonInsert = true;
-                this.isShowButtonUpdate = false;
-            } else {
-                this.isShowButtonInsert = false;
-                this.isShowButtonUpdate = true;
-            }
-        },
-        closeModify: function () {
-            this.isShowSelect = true;
-            this.isShowModify = false;
-        },
-        callFind: function () {
-            Swal.fire({
-                text: "Loading.......",
-                allowOutsideClick: false,
-                showConfirmButton: false
-            });
-
-            if (this.findId === "") {
-                this.findId = null;
-            }
-            if (this.findName === "") {
-                this.findName = null;
-            }
-            let request = {
-                start: 0,
-                rows: 100,
-
-                id: this.findId,
-                name: this.findName,
-            };
-
-            let vm = this;
-            axios.post(contextPath + "/pages/ajax/products/find", request).then(function (response) {
-                let count = response.data.count;
-                vm.foundItems = response.data.list;
-                vm.isShowTableFooter = (count === 0 || vm.foundItems.length === 0);
-
-                setTimeout(function () {
-                    Swal.close();
-                }, 500);
-            }).catch(function (error) {
-                console.log("error", error);
-                Swal.fire({
-                    icon: "error",
-                    text: "查詢錯誤：" + error.message,
-                    confirmButtonText: "確定"
-                });
-            }).finally(function () {
-
-            });
-        },
-        callCreate: function () {
-            Swal.fire({
-                text: "Loading.......",
-                allowOutsideClick: false,
-                showConfirmButton: false
-            });
-
-            if (this.id === "") {
-                this.id = null;
-            }
-            if (this.name === "") {
-                this.name = null;
-            }
-            if (this.price === "") {
-                this.price = null;
-            }
-            if (this.make === "") {
-                this.make = null;
-            }
-            if (this.expire === "") {
-                this.expire = null;
-            }
-            let request = {
-                id: this.id,
-                name: this.name,
-                price: this.price,
-                make: this.make,
-                expire: this.expire,
-            }
-
-            let vm = this;
-            axios.post(contextPath + "/pages/ajax/products", request).then(function (response) {
-                if (response.data.success) {
-                    console.log("message", response.data.message);
-                    Swal.fire({
-                        icon: "success",
-                        text: response.data.message,
-                        allowOutsideClick: false,
-                        confirmButtonText: "確定"
-                    }).then(function (result) {
-                        if (result.isConfirmed) {
-                            vm.closeModify();
-                            vm.callFind();
-                        }
-                    });
-                } else {
-                    Swal.fire({
-                        icon: "error",
-                        text: response.data.message,
-                        confirmButtonText: "確定"
-                    });
-                }
-            }).catch(function (error) {
-                Swal.fire({
-                    icon: "error",
-                    text: "查詢錯誤：" + error.message,
-                    confirmButtonText: "確定"
-                });
-            }).finally(function () {
-
-            });
-        },
-        callRemove: function (id) {
-            let vm = this;
-            Swal.fire({
-                icon: "question",
-                text: "確定要刪除嗎？",
-                allowOutsideClick: false,
-                confirmButtonText: "確定",
-                showCancelButton: true,
-                cancelButtonText: "取消"
-            }).then(function (result) {
-                if (result.isConfirmed) {
-                    Swal.fire({
-                        text: "Loading.......",
-                        allowOutsideClick: false,
-                        showConfirmButton: false
-                    });
-                    axios.delete(contextPath + "/pages/ajax/products/" + id).then(function (response) {
-                        if (response.data.success) {
-                            Swal.fire({
-                                icon: "success",
-                                text: response.data.message,
-                                confirmButtonText: "確定"
-                            }).then(function (result) {
-                                if (result.isConfirmed) {
-
-                                    vm.callFind();
-                                }
-                            });
-                        } else {
-                            Swal.fire({
-                                icon: "error",
-                                text: response.data.message,
-                                confirmButtonText: "確定"
-                            });
-                        }
-                    }).catch(function (error) {
-                        Swal.fire({
-                            icon: "error",
-                            text: "查詢錯誤：" + error.message,
-                            confirmButtonText: "確定"
-                        });
-                    }).finally(function () {
-
-                    });
-                }
-            });
-        },
-        callFindById: function (id) {
-            Swal.fire({
-                text: "Loading.......",
-                allowOutsideClick: false,
-                showConfirmButton: false
-            });
-
-            let vm = this;
-            axios.get(contextPath + "/pages/ajax/products/" + id).then(function (response) {
-                vm.showModify("update");
-                if (response.data.list) {
-                    vm.id = response.data.list[0].id;
-                    vm.name = response.data.list[0].name;
-                    vm.price = response.data.list[0].price;
-                    vm.make = response.data.list[0].make;
-                    vm.expire = response.data.list[0].expire;
-                }
-                setTimeout(function () {
-                    Swal.close();
-                }, 500);
-            }).catch(function (error) {
-                console.log("error1", error);
-                Swal.fire({
-                    icon: "error",
-                    text: "查詢錯誤：" + error.message,
-                    confirmButtonText: "確定"
-                }).then(function (result) {
-
-                });
-            });
-        },
-        callModify: function () {
-            Swal.fire({
-                text: "Loading.......",
-                allowOutsideClick: false,
-                showConfirmButton: false
-            });
-
-            if (this.id === "") {
-                this.id = null;
-            }
-            if (this.name === "") {
-                this.name = null;
-            }
-            if (this.price === "") {
-                this.price = null;
-            }
-            if (this.make === "") {
-                this.make = null;
-            }
-            if (this.expire === "") {
-                this.expire = null;
-            }
-            let request = {
-                id: this.id,
-                name: this.name,
-                price: this.price,
-                make: this.make,
-                expire: this.expire,
-            }
-            let vm = this;
-            axios.put(contextPath + "/api/house/find" + this.id, request).then(function (response) {
-                console.log("response", response);
-                if (response.data.success) {
-                    Swal.fire({
-                        icon: "success",
-                        text: response.data.message,
-                        confirmButtonText: "確定"
-                    }).then(function (result) {
-                        if (result.isConfirmed) {
-                            vm.closeModify();
-                            vm.callFind();
-                        }
-                    });
-                } else {
-                    Swal.fire({
-                        icon: "error",
-                        text: response.data.message,
-                        confirmButtonText: "確定"
-                    });
-                }
-            }).catch(function (error) {
-                Swal.fire({
-                    icon: "error",
-                    text: "查詢錯誤：" + error.message,
-                    confirmButtonText: "確定"
-                });
-            }).finally(function () {
-
-            });
-        },
-    },
+const mappedHouses = computed(() => {
+  return houses.value.map((house) => {
+    return {
+      ...house,
+      hasWifi: mapBoolToString(house.hasWifi),
+      hasTV: mapBoolToString(house.hasTV),
+      hasKitchen: mapBoolToString(house.hasKitchen),
+      hasLaundry: mapBoolToString(house.hasLaundry),
+      hasParkingLot: mapBoolToString(house.hasParkingLot),
+      hasAirconditioner: mapBoolToString(house.hasAirconditioner),
+      hasPersonalSpace: mapBoolToString(house.hasPersonalSpace),
+      hasPool: mapBoolToString(house.hasPool),
+      hasGym: mapBoolToString(house.hasGym),
+      isDeleted: mapBoolToString(house.isDeleted),
+    };
+  });
 });
-app.mount("#app");
+
+const openUpdateModal = (houseId) => {
+  currentHouseToUpdate = houseId;
+  isUpdateModalVisible.value = true;
+};
+
+const closeUpdateModal = () => {
+  currentHouseToUpdate = null;
+  isUpdateModalVisible.value = false;
+};
+
+const confirmDelete = (houseId) => {
+  Swal.fire({
+    title: '你確定要刪除嗎',
+    text: '這沒辦法復原資料的',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#d33',
+    cancelButtonColor: '#3085d6',
+    confirmButtonText: 'Yes'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      // Call your Spring Boot delete API here with houseId using Axios
+      axios.delete(`http://localhost:8080/api/house/delete/${houseId}`)
+        .then(() => {
+          // Show a success message
+          Swal.fire('刪除成功!', '你的資料已刪除成功', 'success');
+          // Fetch the updated list of houses
+          fetchHouses();
+        })
+        .catch((error) => {
+          console.error('刪除失敗', error);
+          // Show an error message if deletion fails
+          Swal.fire('刪除失敗', '你在刪除時產生錯誤', 'error');
+        });
+    }
+  });
+};
+
+onMounted(() => {
+  // Fetch data from your Spring Boot backend when the component is mounted
+  fetchHouses();
+});
 </script>
     
 <style scoped>
 .tableDiv {
-    width: 90%;
-    margin: auto;
-    text-align: center;
-    overflow: scroll;
+  width: 90%;
+  margin: auto;
+  text-align: center;
+  overflow: scroll;
 }
 
 .table tr:nth-child(even) {
-    --bs-table-bg: #FFD966;
+  --bs-table-bg: #FFD966;
+}
+
+table {
+  border: 3px solid #0E2E50;
+}
+
+table td {
+  border: 1px solid #0E2E50;
+}
+
+.modal {
+  z-index: 1050; /* or a higher value */
 }
 </style>
