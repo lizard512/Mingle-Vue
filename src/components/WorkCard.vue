1cee9a4c2963d52d9cf6345f7637842e1e5dd881
lizard512 <lizard@smail.nchu.edu.tw>
2024-01-29 14:59:44 +0800

    Refactor WorkCard.vue and UserProfile.vue components

--- src/components/WorkCard.vue
+++ src/components/WorkCard.vue
@@ -3,35 +3,37 @@
         <div class="row g-4">
             <div class="col-xxl-2 col-xl-3 col-lg-4 col-md-6 animate__animated animate__fadeIn" v-for="work in works"
                 :key="work.workid">
-                <div class="list-item rounded overflow-hidden">
-                    <div class="position-relative overflow-hidden">
-                        <img class="img-fluid" src="@images/台東熱氣球活動.jpg" :src="work.photo"
-                            @error="work.photo = '@images/grey.jpg'" :alt="work.name">
-                        <div class="bg-info rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">
-                            {{ work.worktype }}</div>
-                        <div class="bg-success rounded-top text-white position-absolute start-0 bottom-0 mx-4 pt-1 px-3">
-                            {{ work.city }}</div>
+                <router-link class="router-link" :to="`/work-detail/${work.workid}`">
+                    <div class="list-item rounded overflow-hidden">
+                        <div class="position-relative overflow-hidden">
+                            <img class="img-fluid" src="@images/台東熱氣球活動.jpg" :src="work.photo"
+                                @error="work.photo = '@images/grey.jpg'" :alt="work.name">
+                            <div class="bg-info rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">
+                                {{ work.worktype }}</div>
+                            <div
+                                class="bg-success rounded-top text-white position-absolute start-0 bottom-0 mx-4 pt-1 px-3">
+                                {{ work.city }}</div>
+                        </div>
+                        <div class="p-4 pb-0">
+                            <p class="text-truncate h5" href="">{{ work.name }}</p>
+                            <p class="text-truncate"><i class="fa fa-map-marker me-2"></i>{{
+                                work.address }}</p>
+                            <!-- <p>{{ work.description }}</p> -->
+                        </div>
+                        <div class="d-flex border-top">
+                            <small class="flex-fill text-center border-end py-2"><i class="fa fa-calendar me-2"></i>{{
+                                work.startDate.toString().substring(0, 10) }} ~
+                                {{ work.endDate.toString().substring(0, 10) }}</small>
+                        </div>
+                        <div class="d-flex border-top">
+                            <small class="flex-fill text-center border-end py-2"><i class="fa fa-user me-2"></i>{{
+                                work.attendance }} /
+                                {{ work.maxAttendance }} 人已報名</small>
+                            <small class="flex-fill text-center py-2"><i class="fa fa-solid fa-eye me-2"></i>{{
+                                work.views }} 次瀏覽</small>
+                        </div>
                     </div>
-                    <div class="p-4 pb-0">
-                        <p class="text-truncate h5" href="">{{ work.name }}</p>
-                        <p class="text-truncate"><i class="fa fa-map-marker me-2"></i>{{
-                            work.address }}</p>
-                        <!-- <p>{{ work.description }}</p> -->
-                    </div>
-                    <div class="d-flex border-top">
-                        <small class="flex-fill text-center border-end py-2"><i class="fa fa-calendar me-2"></i>{{
-                            work.startDate.toString().substring(0, 10) }} ~
-                            {{ work.endDate.toString().substring(0, 10) }}</small>
-                    </div>
-                    <div class="d-flex border-top">
-                        <small class="flex-fill text-center border-end py-2"><i class="fa fa-user me-2"></i>{{
-                            work.attendance }} /
-                            {{ work.maxAttendance }} 人已報名</small>
-                        <small class="flex-fill text-center py-2"><i class="fa fa-solid fa-eye me-2"></i>{{
-                            work.views }} 次瀏覽</small>
-                    </div>
-                </div>
-
+                </router-link>
             </div>
         </div>
     </div>
@@ -66,5 +68,10 @@ const store = useWorkStore();
 
 .list-item .border-end {
     border-right: 1px dashed rgba(0, 185, 142, .3) !important;
+}
+
+.router-link {
+    color: inherit;
+    text-decoration: none;
 }
 </style>
