79bf913a409d313162d126e6c31a4b0ee398dc8b
lizard512 <lizard@smail.nchu.edu.tw>
2024-02-02 09:58:25 +0800

    Refactor Toggle and WorkDeck components

--- src/components/WorkDeck.vue
+++ src/components/WorkDeck.vue
@@ -2,35 +2,34 @@
     <swiper :style="{
         '--swiper-navigation-color': '#fff',
         '--swiper-pagination-color': '#fff',
-    }" :effect="'cards'" :grabCursor="true" :navigation="true" :modules="modules" class="mySwiper">
-        <swiper-slide v-for="work in works" :key="work.workid">
+    }" :effect="'cards'" :grabCursor="true" :navigation="true" :modules="modules"
+        :autoplay="{delay: 2500, disableOnInteraction: false }" class="mySwiper">
+        <swiper-slide class="list-item rounded overflow-hidden placeholder-glow" v-for="work in works" :key="work.workid">
             <router-link class="router-link" :to="`/work-detail/${work.workid}`">
-                <div class="list-item rounded overflow-hidden placeholder-glow">
-                    <div class="position-relative overflow-hidden">
-                        <img v-if="work.photosBase64.length" class="img-fluid" :src="work.photosBase64" :alt="work.name">
-                        <img v-else class="img-fluid" src="@images/ImageNotFound.jpg" :alt="work.name">
-                        <div class="bg-info rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">
-                            {{ work.worktype }}</div>
-                        <div class="bg-success rounded-top text-white position-absolute start-0 bottom-0 mx-4 pt-1 px-3">
-                            {{ work.city }}</div>
-                    </div>
-                    <div class="p-4 pt-3 pb-0">
-                        <p class="text-truncate h5">{{ work.name }}</p>
-                        <p class="text-truncate"><i class="fa fa-map-marker me-2"></i>{{
-                            work.address }}</p>
-                    </div>
-                    <div class="d-flex border-top">
-                        <small class="flex-fill text-center py-2"><i class="fa fa-calendar me-2"></i>{{
-                            work.startDate.toString().substring(0, 10) }} ~
-                            {{ work.endDate.toString().substring(0, 10) }}</small>
-                    </div>
-                    <div class="d-flex border-top">
-                        <small class="flex-fill text-center py-2 border-end"><i class="fa fa-user me-2"></i>{{
-                            work.attendance }} /
-                            {{ work.maxAttendance }} 人已報名</small>
-                        <small class="flex-fill text-center py-2"><i class="fa fa-solid fa-eye me-2"></i>{{
-                            work.views }} 次瀏覽</small>
-                    </div>
+                <div class="position-relative overflow-hidden">
+                    <img v-if="work.photosBase64.length" class="img-fluid" :src="work.photosBase64" :alt="work.name">
+                    <img v-else class="img-fluid" src="@images/ImageNotFound.jpg" :alt="work.name">
+                    <div class="bg-info rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">
+                        {{ work.worktype }}</div>
+                    <div class="bg-success rounded-top text-white position-absolute start-0 bottom-0 mx-4 pt-1 px-3">
+                        {{ work.city }}</div>
+                </div>
+                <div class="p-4 pt-3 pb-0">
+                    <p class="text-truncate h5">{{ work.name }}</p>
+                    <p class="text-truncate"><i class="fa fa-map-marker me-2"></i>{{
+                        work.address }}</p>
+                </div>
+                <div class="d-flex border-top">
+                    <small class="flex-fill text-center py-2"><i class="fa fa-calendar me-2"></i>{{
+                        work.startDate.toString().substring(0, 10) }} ~
+                        {{ work.endDate.toString().substring(0, 10) }}</small>
+                </div>
+                <div class="d-flex border-top">
+                    <small class="flex-fill text-center py-2 border-end"><i class="fa fa-user me-2"></i>{{
+                        work.attendance }} /
+                        {{ work.maxAttendance }} 人已報名</small>
+                    <small class="flex-fill text-center py-2"><i class="fa fa-solid fa-eye me-2"></i>{{
+                        work.views }} 次瀏覽</small>
                 </div>
             </router-link>
         </swiper-slide>
@@ -46,8 +45,8 @@ import 'swiper/css';
 import 'swiper/css/effect-cards';
 import 'swiper/css/navigation';
 // import required modules
-import { EffectCards, Pagination, Navigation } from 'swiper/modules';
-const modules = [EffectCards, Navigation];
+import {  Autoplay, EffectCards, Navigation } from 'swiper/modules';
+const modules = [ Autoplay, EffectCards, Navigation];
 
 //// 引用函式庫
 import { ref, onMounted } from 'vue';
@@ -167,10 +166,5 @@ const loadWork = async () => {
 
 .fa-gratipay {
     font-size: 2em;
-}
-
-
-.swiper {
-    width: 50%;
 }
 </style>
