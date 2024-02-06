7364507d9e1b60dc959e8e5fe46a53b809669251
LiTseLin <lizard@smail.nchu.edu.tw>
2024-02-07 03:02:01 +0800

    Refactor home page layout and styles

--- src/views/common/Home.vue
+++ src/views/common/Home.vue
@@ -1,42 +1,44 @@
 <template>
   <!-- Header Start -->
-  <div class="container-fluid header p-0">
-    <div class="row g-0 align-items-center flex-column-reverse flex-md-row">
-      <div class="col-md-6 p-5 mt-lg-5 text-center">
-        <h1 class="display-5 animated fadeIn mb-4 text-black-var">一點 <span class="text-info ">幫助</span>
-          ，一晚<span class="text-info "> 住宿。</span>
+  <div class="container p-0">
+    <div class="row g-0 align-items-center title-text">
+      <div class="col-xl-6 p-5 text-center animate__animated animate__fadeIn">
+        <h1 class="display-5 mb-4 text-black-var">一點 <span class="text-info">幫助</span>
+          ，一晚<span class="text-info"> 住宿。</span>
         </h1>
-        <p class="animated fadeIn mb-4 pb-2 text-dark-var">全台 26 縣市提供打工換宿</p>
-        <a href="" class="btn btn-secondary py-3 px-5 me-3 animated fadeIn">了解詳情</a>
+        <p class="display-6 mb-4 pb-2 text-dark-var">全台 26 縣市提供打工換宿</p>
+        <a href="" class="btn btn-secondary py-3 px-5">了解詳情</a>
       </div>
-      <swiper :style="{
-        '--swiper-navigation-color': '#fff',
-        '--swiper-pagination-color': '#fff',
-      }" :centeredSlides="true" :loop="true" :navigation="true" :modules="modules" :pagination="{ clickable: true }"
-        :autoplay="{ delay: 2500, disableOnInteraction: true }" class="mySwiper">
-        <swiper-slide><img src="@images/carousel-1.jpg" class="w-100" alt="..."></swiper-slide>
-        <swiper-slide><img src="@images/carousel-2.jpg" class="w-100" alt="..."></swiper-slide>
-      </swiper>
+      <div class="col-xl-6">
+        <swiper :style="{
+          '--swiper-navigation-color': '#fff',
+          '--swiper-pagination-color': '#fff',
+        }" :centeredSlides="true" :loop="true" :navigation="true" :modules="modules" :pagination="{ clickable: true }"
+          :autoplay="{ delay: 2500, disableOnInteraction: true }" class="mySwiper">
+          <swiper-slide><img src="@images/carousel-1.jpg" class="w-100" alt="..."></swiper-slide>
+          <swiper-slide><img src="@images/carousel-2.jpg" class="w-100" alt="..."></swiper-slide>
+        </swiper>
+      </div>
     </div>
   </div>
   <!-- Header End -->
   <!--Work Card Start-->
-  <div class="container-fluid p-5">
+  <div class="container">
     <div class="row">
-      <div class="col-xxl-3 col-lg-6 text-center h-100">
-        <h2 class="mx-3">最多人看</h2>
+      <div class="col-xxl-3 col-lg-6 text-center h-100 p-5">
+        <h2>最多人看</h2>
         <WorkDeck />
       </div>
-      <div class="col-xxl-3 col-lg-6 text-center h-100">
-        <h2 class="mx-3">最新上架</h2>
+      <div class="col-xxl-3 col-lg-6 text-center h-100 p-5">
+        <h2>最新上架</h2>
         <WorkDeck property="createdAt" />
       </div>
-      <div class="col-xxl-3 col-lg-6 text-center h-100">
-        <h2 class="mx-3">即將截止</h2>
+      <div class="col-xxl-3 col-lg-6 text-center h-100 p-5">
+        <h2>即將截止</h2>
         <WorkDeck property="endDate" direction="ASC" />
       </div>
-      <div class="col-xxl-3 col-lg-6 text-center h-100">
-        <h2 class="mx-3">搶手項目</h2>
+      <div class="col-xxl-3 col-lg-6 text-center h-100 p-5">
+        <h2>搶手項目</h2>
         <WorkDeck property="attendance" />
       </div>
     </div>
@@ -52,7 +54,7 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 // import required modules
-import { Autoplay, Pagination, Navigation} from 'swiper/modules';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 const modules = [Autoplay, Pagination, Navigation];
 
 //// 引用元件
@@ -63,12 +65,16 @@ import WorkDeck from '@components/WorkDeck.vue';
 
 <!-- Customized CSS -->
 <style scoped>
+.title-text {
+  background-color: var(--light);
+}
+
 .text-black-var {
   color: var(--black);
 }
 
 .swiper {
-  width: 50%;
+  width: 100%;
   height: 50%;
 }
 
@@ -78,6 +84,7 @@ import WorkDeck from '@components/WorkDeck.vue';
 }
 
 .swiper-slide {
-  align-items: center; /* add this will all relevant prefixes */
+  align-items: center;
+  /* add this will all relevant prefixes */
 }
 </style>
