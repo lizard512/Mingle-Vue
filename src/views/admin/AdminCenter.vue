e01cbab3e0bef0104e15875638ac7905b49c3bad
lizard512 <lizard@smail.nchu.edu.tw>
2024-02-06 11:27:15 +0800

    Update AdminCenter.vue with localized text and icons

--- src/views/admin/AdminCenter.vue
+++ src/views/admin/AdminCenter.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="vh-100">
         <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-            <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
+            <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Mingle 管理者大平台</a>
             <div class="navbar-nav">
                 <div class="nav-item text-nowrap">
-                    <RouterLink to="/" class="nav-link px-3">退出管理者介面</RouterLink>
+                    <RouterLink to="/" class="nav-link px-3"><i class="fa-solid fa-person-through-window me-2"></i>退出管理者介面</RouterLink>
                 </div>
             </div>
         </header>
@@ -16,10 +16,13 @@
                         <ul class="nav flex-column">
                             <li class="nav-item">
                                 <RouterLink class="nav-link active" :to="{ name: 'AdminHome' }">
-                                    <span class="fa fa-solid fa-database"></span>
+                                    <i class="fa fa-solid fa-clipboard-list"></i>
                                     Dashboard
                                 </RouterLink>
                             </li>
+                            <h6 class="sidebar-heading px-3 mt-4 mb-1 text-light"><i class="fa fa-solid fa-database"></i>
+                                Data Management
+                            </h6>
                             <li class="nav-item">
                                 <RouterLink class="nav-link" :to="{ name: 'AdminUser' }">
                                     <i class="fa fa-solid fa-users"></i>
@@ -34,49 +37,52 @@
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="#">
+                                    <span class="fa fa-house"></span>
+                                    Houses
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
                                     <span class="fa fa-shopping-cart"></span>
                                     Orders
                                 </a>
                             </li>
                         </ul>
 
-                        <h6
-                            class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                            <span>Saved reports</span>
-                            <a class="link-secondary" href="#" aria-label="Add a new report">
-                                <span class="fa fa-plus-circle"></span>
-                            </a>
+                        <h6 class="sidebar-heading px-3 mt-4 mb-1 text-light"><i class="fa-solid fa-screwdriver-wrench"></i>
+                            Admin Function
                         </h6>
                         <ul class="nav flex-column mb-2">
                             <li class="nav-item">
                                 <a class="nav-link" href="#">
-                                    <span class="fa fa-file-text"></span>
-                                    Current month
+                                    <i class="fa fa-solid fa-check-to-slot"></i>
+                                    處理工作檢舉
                                 </a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="#">
-                                    <span class="fa fa-file-text"></span>
-                                    Year-end sale
+                                    <i class="fa fa-solid fa-user-check"></i>
+                                    封鎖使用者帳戶
                                 </a>
                             </li>
                         </ul>
                         <hr>
-                        <div class="dropdown">
-                            <a href="#" class="d-flex align-items-center text-decoration-none dropdown-toggle"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                <img src="https://github.com/mdo.png" alt="" width="32" height="32"
-                                    class="rounded-circle me-2">
-                                <strong>mdo</strong>
+                        <!-- Sidebar user info -->
+                        <div class="dropdown dropup p-3 user-info">
+                            <a href="#" class="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src="@images/user/woman.png" alt="" width="32" height="32" class="rounded-circle me-2">
+                                <strong>{{ user.name }}</strong>
                             </a>
                             <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-                                <li><a class="dropdown-item" href="#">New project...</a></li>
-                                <li><a class="dropdown-item" href="#">Settings</a></li>
-                                <li><a class="dropdown-item" href="#">Profile</a></li>
+                                <li><a class="dropdown-item" href="#"><i class="fa-solid fa-gear me-2"></i>設定</a></li>
+                                <li><a class="dropdown-item" href="#"><i class="fa-solid fa-id-card me-2"></i>個人檔案</a></li>
                                 <li>
                                     <hr class="dropdown-divider">
                                 </li>
-                                <li><a class="dropdown-item" href="#">Sign out</a></li>
+                                <li><a class="dropdown-item" href="#"><i class="fa-solid fa-right-from-bracket me-2"></i>登出</a></li>
+                                <li class="nav-item text-nowrap">
+                    <RouterLink to="/" class="nav-link px-3"><i class="fa-solid fa-person-through-window me-2"></i>退出管理者介面</RouterLink>
+                </li>
                             </ul>
                         </div>
                     </div>
@@ -91,33 +97,37 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+onMounted(async () => {
+
+    await loadUserData();
+});
+
+const userID = localStorage.getItem('userID');
+const user = ref({});
+
+const loadUserData = async () => {
+    try {
+        const response = await axios.get(`${import.meta.env.VITE_APP_API_URL}/api/volunteerDetail/${userID}`);
+        user.value = response.data;
+    } catch (error) {
+        console.error('Failed to fetch user data:', error);
+    }
+}
+
 </script>
     
 <style scoped>
-/*
- * Sidebar
- */
-
 .sidebar {
     position: fixed;
     top: 0;
-    /* rtl:raw:
-  right: 0;
-  */
     bottom: 0;
-    /* rtl:remove */
     left: 0;
     z-index: 100;
-    /* Behind the navbar */
     padding: 48px 0 0;
-    /* Height of navbar */
     box-shadow: inset -1px 0 0 rgba(0, 0, 0, .1);
-}
-
-@media (max-width: 767.98px) {
-    .sidebar {
-        top: 5rem;
-    }
 }
 
 .sidebar-sticky {
@@ -131,32 +141,29 @@
 }
 
 .sidebar .nav-link {
-    font-weight: 500;
     color: var(--light);
 }
 
 .sidebar .nav-link .fa {
     margin-right: 4px;
     color: #727272;
+    background-color: #eee;
+    padding: 5px;
+    border-radius: 4px;
+    height: 30px;
+    width: 30px;
+    text-align: center;
 }
 
 .sidebar .nav-link.active {
-    color: #2470dc;
+    color: var(--primary) !important;
 }
 
-.sidebar .nav-link:hover .fa,
-.sidebar .nav-link.active .fa {
-    color: inherit;
+.sidebar .nav-link:hover {
+    color: var(--secondary);
 }
 
-.sidebar-heading {
-    font-size: .75rem;
-    text-transform: uppercase;
-}
 
-/*
- * Navbar
- */
 
 .navbar-brand {
     padding-top: .75rem;
@@ -171,4 +178,14 @@
     right: 1rem;
 }
 
+.user-info {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    /* Adjust this value to align with your sidebar */
+}
+.user-info .fa-solid {
+    width: 20px;
+    height: 20px;
+}
 </style>
