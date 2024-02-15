476cb241965a7fe6e021ac29f90a918a1fc71054
DESKTOP-RAE7E6C\User <spi2013416@gmail.com>
2024-02-15 11:26:56 +0800

    a

--- src/views/user/Payment.vue
+++ src/views/user/Payment.vue
@@ -1,18 +1,19 @@
 <template>
-    <br>    
-    <button @click="getPaid">送出</button>
+    <br><el-button type="success" round @click="getPaid">付款</el-button>
+    <p>{{ props.amount }}</p>
 </template>
     
 <script setup>
-    import{ ref } from 'vue';
-    let path = import.meta.env.VITE_APP_API_URL;
-    const amount = ref('');
-    function getPaid(){
-        amount.value = 100;
-        window.location.href = `${path}/ecpayCheckout/${amount.value}`;
-    }
+import { ref } from 'vue';
+let path = import.meta.env.VITE_APP_API_URL;
+const amount2 = ref('');
+const props = defineProps({
+    amount: Number,
+})
+function getPaid() {
+    amount2.value = 100;
+    window.location.href = `${path}/ecpayCheckout/${amount2.value}`;
+}
 </script>
     
-<style scoped>
-    
-</style>
+<style scoped></style>
