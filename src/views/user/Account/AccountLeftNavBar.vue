6c716175e1dcc5ef8402e04822ad38d9cc2ff558
DESKTOP-RAE7E6C\User <spi2013416@gmail.com>
2024-02-18 14:27:39 +0800

    改密碼放到navber

--- src/views/user/Account/AccountLeftNavBar.vue
+++ src/views/user/Account/AccountLeftNavBar.vue
@@ -11,16 +11,139 @@
         </div> -->
         <div class="row my-5" @click="clickHandler(isShow = 3)"><a href="#" class="styleColor ">
                 <i class="fa-brands fa-gratipay" />心願清單</a></div>
+        <div class="row my-5 styleColor" @click="ChangesPassword"><i class="fa-solid fa-lock">更改密碼</i></div>
     </div>
 </template>
 
 <script setup >
+import Swal from 'sweetalert2';
+import axios from 'axios';
+import { useRouter } from 'vue-router'
+
+
+const router = useRouter();
 
 const emit = defineEmits(["xyzClick"])
 const clickHandler = showVeiw => {
 
     emit("xyzClick", showVeiw)
 }
+
+const getuserid =
+    () => {
+        const id = localStorage.getItem('userID');
+        return id;
+    }
+let changePassword = "";
+const ChangesPassword = () => {
+    Swal.fire({
+        icon: 'question',
+        text: '是否要修改密碼',
+        allowOutsideClick: false,
+        confirmButtonText: "確定",
+        showCancelButton: true,
+        cancelButtonText: "再想想",
+    }).then(function (result) {
+        if (result.isConfirmed) {
+
+            const { value: oldPassword } = Swal.fire({
+                title: "請輸入你的舊密碼",
+                input: "password",
+                inputPlaceholder: "Enter your old password",
+                allowOutsideClick: false,
+                showCloseButton: true,
+                inputAttributes: {
+                    autocapitalize: "off"
+                },
+                showDenyButton: true,
+                denyButtonText: "重寄驗證信來重設密碼",
+                confirmButtonText: "確認舊密碼",
+                showLoaderOnConfirm: true,
+                preConfirm: async (oldPassword) => {
+                    try {
+                        if (oldPassword === "" || oldPassword === null || oldPassword === undefined) {
+                            return Swal.showValidationMessage(`請輸入密碼`);
+                        }
+                        const response = await axios.get(`${import.meta.env.VITE_APP_API_URL}/login/byId/${getuserid()}/${oldPassword}`)
+                            .then(function (response) {
+                                if (response.data.success) {
+                                    changePassword = oldPassword;
+                                    Swal.fire({
+                                        icon: "success",
+                                        text: "更新成功",
+                                        confirmButtonText: "確定"
+                                    })
+                                } else {
+                                    return Swal.showValidationMessage(`舊密碼錯誤，請確認後再重新輸入`);
+                                }
+                            }).catch(function (error) {
+                                Swal.fire({
+                                    icon: "error",
+                                    title: "糟糕...",
+                                    text: "操作失敗" + error,
+                                    confirmButtonText: "確認"
+                                })
+                            })
+                    } catch (error) {
+                        Swal.showValidationMessage(`Request failed: ${error}`);
+                    }
+                },
+                allowOutsideClick: () => !Swal.isLoading()
+            }).then((result) => {
+                if (result.isConfirmed) {
+
+
+                    const { value: formValues } = Swal.fire({
+                        title: `請輸入你的新密碼`,
+                        html: `<input type="password" id="swal-input1" class="swal2-input" placeholder="new password" >
+                                <input type="password" id="swal-input2" class="swal2-input" placeholder="password again">`,
+                        focusConfirm: false,
+                        preConfirm: async () => {
+                            if (document.getElementById("swal-input1").value === "" || document.getElementById("swal-input2").value === "") {
+                                return Swal.showValidationMessage(`請輸入新密碼`);
+                            }
+                            else if (document.getElementById("swal-input1").value !==
+                                document.getElementById("swal-input2").value) {
+                                return Swal.showValidationMessage(`請確認密碼一致`);
+                            } else if (document.getElementById("swal-input1").value ===
+                                document.getElementById("swal-input2").value) {
+                                if (changePassword === document.getElementById("swal-input1").value) {
+                                    return Swal.showValidationMessage(`請勿與舊密碼一致`);
+                                }
+                                const data = {
+                                    id: getuserid(),
+                                    password: document.getElementById("swal-input1").value
+                                }
+                                await axios.patch(`${import.meta.env.VITE_APP_API_URL}/resetPassword/byId`, data).then(function (response) {
+                                    if (response.data.success) {
+                                        Swal.fire({
+                                            icon: "success",
+                                            text: "更新成功",
+                                            confirmButtonText: "確定"
+                                        })
+                                    } else {
+                                        return Swal.showValidationMessage(`密碼更新失敗，請確認後再重新輸入`);
+                                    }
+                                }).catch(function (error) {
+                                    Swal.fire({
+                                        icon: "error",
+                                        title: "糟糕...",
+                                        text: "操作失敗" + error,
+                                        confirmButtonText: "確認"
+                                    })
+                                })
+                            }
+                        }
+                    });
+
+                } else if (result.isDenied) {
+                    router.push({ name: "ForgetPassword" });
+                }
+            });
+        }
+    });
+}
+
 </script>
     
 <style scoped>
